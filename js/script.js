a21cbb9c9a9952ca3275da05b7bfd72ee59da789
sayqieese <angela2001.cts08@nctu.edu.tw>
2021-06-06 22:09:56 +0800

    修改

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// confirm() 
+function c() {
+  m = confirm('這是彈跳確認視窗');
+  // 可透過真假值去做不同的變化
+  if(m == true)
+    alert("按下確定");
+}
+// prompt () 
+function p() {
+  prompt('這是輸入訊息視窗');
+  console.log(a);
+}
+
